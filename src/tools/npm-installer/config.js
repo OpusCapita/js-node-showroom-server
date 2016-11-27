14c9fe2489ba902b5a0796b96f6d7378e5331e48
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2016-11-27 15:52:08 +0300

    Update config.js

--- src/tools/npm-installer/config.js
+++ src/tools/npm-installer/config.js
@@ -5,18 +5,14 @@ let semver = require('semver');
 
 module.exports = {
   packages: [
-    // {
-    //   name: 'test-showroom-activity',
-    //   versionsFilter: version => semver.gt(version, '1.0.2')
-    // },
     {
-      name: 'jcatalog-react-reference-search',
-      versionsFilter: version => semver.gt(version, '2.2.2') && !/snapshot/gi.test(version)
+       name: 'test-showroom-activity',
+       versionsFilter: version => semver.gt(version, '1.0.2')
     },
-    // {
-    //   name: 'test-showroom-jcatalog-react-user-profile',
-    //   versionsFilter: version => semver.gt(version, '1.0.7')
-    // }
+    {
+       name: 'test-showroom-jcatalog-react-user-profile',
+       versionsFilter: version => semver.gt(version, '1.0.7')
+    }
   ],
   installationRoot: libPath.resolve(libPath.join(__dirname, '../../../tmp/npm-installer/packages')),
   packagesInfoPath: libPath.resolve(libPath.join(__dirname, '../../../tmp/npm-installer/packages-info.js'))
