ff66db2053446f530b7b02262f516dd4fa8f9274
Dmitry Shienok <dshienok@scand.com>
2018-04-25 12:24:36 +0300

    Created new release branch. [ci skip]

--- src/bin/showroom-scan.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-let makeLocalScan = require('../tools/npm-scanner/make-local-scan').default;
-let componentsPath = process.argv[2];
-makeLocalScan(componentsPath);
--- src/tools/npm-bundler/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let libPath = require('path');
-
-module.exports = {
-  installationRoot: libPath.resolve(__dirname, '../../../tmp/npm-installer/packages'),
-  packagesInfoPath: libPath.resolve(__dirname, '../../../tmp/npm-installer/packages-info.js'),
-  webpackConfigPath: libPath.resolve(__dirname, './webpack.config.js')
-};
